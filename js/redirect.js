79d5cea50e4d5e471e309174da86fe501e71945e
DETJzsb <altlili.aymen@gmail.com>
2025-12-14 21:31:51 +0100

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,26 +1,21 @@
-<script>
-async function redirectUser() {
-const { data: { user } } = await supabase.auth.getUser()
-if (!user) {
-window.location.href = 'login.html'
-return
-}
+(async () => {
+  console.log("redirect.js loaded")
 
+  const { data: { user } } = await supabase.auth.getUser()
 
-const { data } = await supabase
-.from('users')
-.select('roles(code)')
-.eq('id', user.id)
-.single()
+  if (!user) {
+    window.location.href = "login.html"
+    return
+  }
 
+  const { data } = await supabase
+    .from("users")
+    .select("roles(code)")
+    .eq("id", user.id)
+    .single()
 
-const role = data.roles.code
+  const role = data.roles.code
 
-
-if (role === 'agent') window.location.href = 'agent.html'
-if (role === 'admin') window.location.href = 'admin.html'
-}
-
-
-redirectUser()
-</script>
+  if (role === "agent") window.location.href = "agent.html"
+  else if (role === "admin") window.location.href = "admin.html"
+})()
