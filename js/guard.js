7eb0cf7aa6f3dba86412ed67420c130e51be2ef1
DETJzsb <altlili.aymen@gmail.com>
2025-12-14 19:55:10 +0100

    Create guard.js

---
+++ js/guard.js
@@ -0,0 +1,22 @@
+<script>
+async function guardRole(expectedRole) {
+const { data: { user } } = await supabase.auth.getUser()
+if (!user) {
+window.location.href = 'login.html'
+return
+}
+
+
+const { data } = await supabase
+.from('users')
+.select('roles(code)')
+.eq('id', user.id)
+.single()
+
+
+if (data.roles.code !== expectedRole) {
+alert('Access denied')
+window.location.href = 'index.html'
+}
+}
+</script>
