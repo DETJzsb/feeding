7f3b38e94594584b3c1545e241d63e2a994a9144
DETJzsb <altlili.aymen@gmail.com>
2025-12-14 23:38:07 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,9 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://wiovumauoaxrrrsjwkko.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpb3Z1bWF1b2F4cnJyc2p3a2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NTc2NTIsImV4cCI6MjA4MTEzMzY1Mn0._wg6or6Ht00JRtNAYkv5MNHjtocMLNPg2-StMj4Ul68"
+const SUPABASE_URL = "https://wiovumauoaxrrrsjwkko.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpb3Z1bWF1b2F4cnJyc2p3a2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NTc2NTIsImV4cCI6MjA4MTEzMzY1Mn0._wg6or6Ht00JRtNAYkv5MNHjtocMLNPg2-StMj4Ul68";
 
-window.supabase = supabase.createClient(
+export const supabase = createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
-)
+);
