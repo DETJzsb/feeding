e4983dd34ad41a693be6e8cd4d387eb36935b8e9
DETJzsb <altlili.aymen@gmail.com>
2025-12-14 23:44:29 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://wiovumauoaxrrrsjwkko.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpb3Z1bWF1b2F4cnJyc2p3a2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NTc2NTIsImV4cCI6MjA4MTEzMzY1Mn0._wg6or6Ht00JRtNAYkv5MNHjtocMLNPg2-StMj4Ul68";
