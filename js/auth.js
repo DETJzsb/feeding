c82ab6cab47a78da564d847a711646280a9a0cc7
DETJzsb <altlili.aymen@gmail.com>
2025-12-14 21:32:37 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-<script>
+
 async function login(email, password) {
 const { error } = await supabase.auth.signInWithPassword({ email, password })
 if (!error) window.location.href = 'index.html'
@@ -17,4 +17,4 @@ async function logout() {
 await supabase.auth.signOut()
 window.location.href = 'login.html'
 }
-</script>
+
